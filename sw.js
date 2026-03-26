aebef43b07aa9212db1ea32790d49b60f7e9dcf0
robgordon-stack <robertwgordon@gmail.com>
2026-03-26 20:19:11 +1100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for My Movie Collection
 // To update: bump the version number below, then redeploy
-const VERSION = 'v12';
+const VERSION = 'v13';
 const CACHE   = 'my-films-' + VERSION;
 const ASSETS  = [
   '/movies/',
